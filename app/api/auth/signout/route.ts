228835779459b531a55ce0409cdb4f9b48ae6016
Matt Cutler <mattcutler@Matts-MacBook-Air.local>
2025-10-16 18:37:03 -0700

    changed signout

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -6,8 +6,9 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function POST() {
-  const cookieStore = await cookies();
-  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+  const supabase = createRouteHandlerClient({ 
+    cookies: async () => await cookies() 
+  });
   await supabase.auth.signOut();
   return NextResponse.json({ success: true });
 }
